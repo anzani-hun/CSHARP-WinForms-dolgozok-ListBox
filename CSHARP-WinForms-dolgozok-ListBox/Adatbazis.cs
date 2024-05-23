fc8b76e5261b785d983544af8dfe965aadb9d553
anzani-hun <jerikostreet@gmail.com>
2024-05-23 16:53:33 +0200

    Add project files.

---
+++ CSHARP-WinForms-dolgozok-ListBox/Adatbazis.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// MYSQL DATA nuget telepít
+using MySql.Data.MySqlClient;
+
+namespace CSHARP_WinForms_dolgozok_ListBox
+{
+    class Adatbazis
+    {
+        MySqlCommand SqlCommand = null;
+        MySqlConnection connection = null;
+
+        public Adatbazis()
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.UserID = "root";
+            builder.Password = "";
+            builder.Database = "munkasok";
+            connection = new MySqlConnection(builder.ConnectionString);
+            try
+            {
+                kapcsolatNyit();
+                SqlCommand = connection.CreateCommand();
+                kapcsolatZar();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                Environment.Exit(0);
+            }
+        }
+
+        internal List<Dolgozo> getAllDolgozo()
+        {
+            List<Dolgozo> dolgozos = new List<Dolgozo>();
+            SqlCommand.CommandText = "SELECT * FROM `dolgozok`";
+            kapcsolatNyit();
+            using (MySqlDataReader dr = SqlCommand.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    //-- rekordonkénti feldolgozás
+                    Dolgozo dolgozo = new Dolgozo(dr.GetString("nev"), dr.GetString("neme"), dr.GetString("reszleg"), dr.GetInt32("belepesev"), dr.GetInt32("ber"));
+                    dolgozos.Add(dolgozo);
+                }
+            }
+
+            return dolgozos;
+        }
+
+        private void kapcsolatZar()
+        {
+            if (connection.State != System.Data.ConnectionState.Closed)
+            {
+                connection.Close();
+            }
+        }
+
+        private void kapcsolatNyit()
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+
+        internal void insertDolgozo(string nev, string nem, string reszleg, int belepes, int ber)
+        {
+            SqlCommand.CommandText = "INSERT INTO `dolgozok` (`nev`, `neme`, `reszleg`, `belepesev`, `ber`) VALUES ('" + nev + "', '" + nem + "', '" + reszleg + "', " + belepes + ", " + ber + ")";
+            kapcsolatNyit();
+            if (SqlCommand.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show("Sikeres beszúrás!");
+            }
+            else
+            {
+                MessageBox.Show("Sikertelen beszúrás!");
+            }
+        }
+    }
+}
---
+++ CSHARP-WinForms-dolgozok-ListBox/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSHARP_WinForms_dolgozok_ListBox
+{
+    public partial class Form1 : Form
+    {
+        List<Dolgozo> dolgozok = new List<Dolgozo>();
+        Adatbazis db = new Adatbazis();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listaFrissitese();
+        }
+
+        private void listaFrissitese()
+        {
+            listBox_dolgozok.Items.Clear();
+            dolgozok = db.getAllDolgozo();
+            listBox_dolgozok.Items.AddRange(dolgozok.ToArray());
+        }
+
+        private void listBox_dolgozok_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Dolgozo dolgozo = (Dolgozo)listBox_dolgozok.SelectedItem;
+            textBox_Nev.Text = dolgozo.nev;
+            textBox_Nem.Text = dolgozo.neme;
+            textBox_Reszleg.Text = dolgozo.reszleg;
+            numericUpDown_BelepesEve.Value = dolgozo.belepesev;
+            numericUpDown_Ber.Value = dolgozo.ber;
+        }
+
+        private void button_Insert_Click(object sender, EventArgs e)
+        {
+            string nev = textBox_Nev.Text;
+            string nem = textBox_Nem.Text;
+            string reszleg = textBox_Reszleg.Text;
+            int belepes = (int)numericUpDown_BelepesEve.Value;
+            int ber = (int)numericUpDown_Ber.Value;
+            db.insertDolgozo(nev, nem, reszleg, belepes, ber);
+        }
+    }
+}
